4ac428234f4c8ea3a8e228aeaf3bf3047894862d
ShreyashGaur <shreyashgaur28@gmail.com>
2026-04-16 06:58:57 +0530

    Delete city_trips_pipeline/transformations directory

--- city_trips_pipeline/transformations/gold/trips_lucknow.sql
+++
@@ -1,6 +0,0 @@
-CREATE OR REPLACE VIEW city_trips_data.gold.fact_trips_lucknow
-AS (
-SELECT *
-FROM city_trips_data.gold.fact_trips
-WHERE city_id = 'UP01'
-);
--- city_trips_pipeline/transformations/gold/trips_surat.sql
+++
@@ -1,6 +0,0 @@
-CREATE OR REPLACE VIEW city_trips_data.gold.fact_trips_surat
-AS (
-SELECT *
-FROM city_trips_data.gold.fact_trips
-WHERE city_id = 'GJ01'
-);
